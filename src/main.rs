97793c41e300766c03d1eaf3cd3586c553e44197
skye347 <s347419313@gmail.com>
2019-03-31 23:26:20 +0800

    add demo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
 #![feature(futures_api)]
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+use std::str::Bytes;
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
+
+use actix::prelude::*;
+use bytes::buf::BufMut;
+use bytes::BytesMut;
+use tokio::codec::{BytesCodec, FramedWrite};
+use tokio::fs::File;
+use tokio::io::{ReadHalf, WriteHalf};
+use tokio::prelude::*;
+use tokio::prelude::future::Future;
+use tokio::prelude::future::lazy;
+use tokio::prelude::stream::Stream;
+use tokio::sync::mpsc::*;
+
+use crate::os::nix::simple_demo;
 
 pub mod os;
 
+fn test() {
+    let mut w1=os::nix::open_tuntap_device("w1".to_string()).unwrap();
+    let mut w2=os::nix::open_tuntap_device("w2".to_string()).unwrap();
+    let mut w1_clone = w1.try_clone().unwrap();
+    let mut w2_clone = w2.try_clone().unwrap();
+    let (w1_read,_) = tokio::fs::File::from_std(w1).split();
+    let (_,w1_write) = tokio::fs::File::from_std(w1_clone).split();
+    let (w2_read,_) = tokio::fs::File::from_std(w2).split();
+    let (_,w2_write) = tokio::fs::File::from_std(w2_clone).split();
+    let task1 = tokio::io::copy(w1_read,w2_write).map_err(|e|{
+        dbg!(e);
+    }).map(|_|());
+    let task2 = tokio::io::copy(w2_read,w1_write).map_err(|e|{
+        dbg!(e);
+    }).map(|_|());
+    tokio::run(lazy(||{
+        tokio::spawn(task1);
+        tokio::spawn(task2);
+        Ok(())
+    }));
+}
+
 fn main() {
-    os::nix::stream_demo();
+
 }
--- src/os/nix.rs
+++ src/os/nix.rs
@@ -1,104 +1,90 @@
-use libc;
-use std::ffi::{CString, c_void};
-use std::os::raw::{c_short, c_char, c_int};
-use std::mem::size_of;
-use futures::stream::Stream;
-use futures::task::Waker;
-use futures::Poll;
-use bytes::{BytesMut, BufMut};
-use std::pin::Pin;
-use futures::executor::ThreadPool;
+use std::collections::HashMap;
+use std::ffi::{c_void, CString};
+use std::fs::File as StdFile;
 use std::fs::OpenOptions;
-use futures::StreamExt;
-use futures::future::ready;
-use std::io::Error;
+use std::io::{Error, Read};
+use std::marker::PhantomData;
+use std::mem;
+use std::mem::size_of;
+use std::os::raw::{c_char, c_int, c_short};
+use std::os::unix::io::FromRawFd;
+use std::pin::Pin;
+use std::sync::atomic::Ordering;
+use std::sync::Mutex;
+use std::thread;
+use std::thread::sleep;
+use std::thread::ThreadId;
+use std::time::Duration;
 
-const IFNAMSIZ:usize = libc::IFNAMSIZ;
-const ifreq_SIZE:usize = 40;
-const TUNSETIFF:u64 = 0x400454ca;
+use bytes::{BufMut, BytesMut};
+use libc;
+use nix;
+use nix::sys::aio::{aio_suspend, AioCb};
+use nix::sys::signal::{SigAction, sigaction, Signal, SigSet};
+use nix::sys::signal;
+use nix::sys::signal::SaFlags;
+use nix::sys::signal::SigHandler;
+use tokio;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::io::ReadHalf;
+use tokio::prelude::*;
+use tokio::prelude::Stream;
+
+const IFNAMSIZ: usize = libc::IFNAMSIZ;
+const ifreq_SIZE: usize = 40;
+const TUNSETIFF: u64 = 0x400454ca;
 
 #[repr(C)]
 pub struct raw_ifreq {
-    pub ifrn_name: [c_char;IFNAMSIZ],
+    pub ifrn_name: [c_char; IFNAMSIZ],
     pub ifr_flags: c_short,
-    pub padding:[c_char;ifreq_SIZE-IFNAMSIZ-2]
+    pub padding: [c_char; ifreq_SIZE - IFNAMSIZ - 2],
 }
 
 pub struct ifreq {
-    pub name:String,
-    pub flags:c_short
+    pub name: String,
+    pub flags: c_short,
 }
 
 impl ifreq {
-    fn to_raw(&self)->Option<raw_ifreq> {
+    fn to_raw(&self) -> Option<raw_ifreq> {
         let c_str = CString::new(self.name.clone()).unwrap();
         let c_name = c_str.as_bytes();
         if c_name.len() > IFNAMSIZ {
             return None;
         }
         let mut raw = raw_ifreq {
-            ifrn_name:[0;16],
-            ifr_flags:0,
-            padding:[0;ifreq_SIZE-IFNAMSIZ-2]
+            ifrn_name: [0; 16],
+            ifr_flags: 0,
+            padding: [0; ifreq_SIZE - IFNAMSIZ - 2],
         };
         for i in 0..c_name.len() {
-            raw.ifrn_name[i]=c_name[i] as i8;
+            raw.ifrn_name[i] = c_name[i] as i8;
         }
         raw.ifr_flags = self.flags;
         Some(raw)
     }
 
-    pub fn has_flag(&self,flag:ireq_flag)->bool {
+    pub fn has_flag(&self, flag: ireq_flag) -> bool {
         self.flags & (flag as i16) != 0
     }
 
-    pub fn set_flag(mut self,flag:ireq_flag)->Self {
+    pub fn set_flag(mut self, flag: ireq_flag) -> Self {
         self.flags = self.flags | (flag as i16);
         self
     }
 }
 
 pub enum ireq_flag {
-    IFF_TUN=0x0001,
-    IFF_TAP=0x0002,
-    IFF_NO_PI=0x1000
+    IFF_TUN = 0x0001,
+    IFF_TAP = 0x0002,
+    IFF_NO_PI = 0x1000,
 }
 
-pub struct TunTapBiStream {
-    pub fd: c_int,
-    pub buffer: [u8;1024],
-    pub counter :u32
-}
-
-impl Stream for TunTapBiStream {
-    type Item = BytesMut;
-
-    fn poll_next(mut self: Pin<&mut Self>, waker: &Waker) -> Poll<Option<Self::Item>> {
-        let nread = unsafe {
-            libc::read(self.fd,self.buffer.as_mut_ptr() as *mut c_void,1024)
-        };
-        if nread < 0 {
-            if Error::last_os_error().raw_os_error().unwrap()==libc::EAGAIN {
-                waker.wake();
-                return Poll::Pending;
-            }
-            else {
-                return Poll::Ready(None);
-            }
-        }
-        let mut r=BytesMut::with_capacity(nread as usize);
-        r.put_slice(&self.buffer[0..nread as usize]);
-        return Poll::Ready(Some(r));
-    }
-}
-
-pub fn open_tuntap_device(device:String) -> Option<TunTapBiStream> {
-    let s = libc::aiocb {
-        
-    };
+pub fn open_tuntap_device(device: String) -> Option<StdFile> {
     let tun_path = CString::new("/dev/net/tun").unwrap();
     let fd = unsafe {
-        libc::open(tun_path.as_ptr(),libc::O_RDWR)
+        libc::open(tun_path.as_ptr(), libc::O_RDWR)
     };
     if fd < 0 {
         println!("open fail");
@@ -106,31 +92,23 @@ pub fn open_tuntap_device(device:String) -> Option<TunTapBiStream> {
     }
     let mut s = ifreq {
         name: device,
-        flags: 0
+        flags: 0,
     }.set_flag(ireq_flag::IFF_TAP).set_flag(ireq_flag::IFF_NO_PI).to_raw().unwrap();
     let r = unsafe {
-        libc::ioctl(fd,TUNSETIFF, &s)
+        libc::ioctl(fd, TUNSETIFF, &s)
     };
     if r < 0 {
         println!("ioctl err");
         unsafe { libc::close(fd); }
         return None;
     }
-//    unsafe {
-//        let flags = libc::fcntl(fd,libc::F_GETFL,0);
-//        libc::fcntl(fd,libc::F_SETFL,flags | libc::O_NONBLOCK);
-//    }
-    Some(TunTapBiStream {
-        fd,
-        buffer:[0u8;1024],
-        counter:0
-    })
+    Some(unsafe { StdFile::from_raw_fd(fd) })
 }
 
-fn simple_demo() {
+pub fn simple_demo() {
     let tun_path = CString::new("/dev/net/tun").unwrap();
     let fd = unsafe {
-        libc::open(tun_path.as_ptr(),libc::O_RDWR)
+        libc::open(tun_path.as_ptr(), libc::O_RDWR)
     };
     if fd < 0 {
         println!("open fail");
@@ -138,34 +116,25 @@ fn simple_demo() {
     }
     let mut s = ifreq {
         name: "w1".to_string(),
-        flags: 0
+        flags: 0,
     }.set_flag(ireq_flag::IFF_TAP).set_flag(ireq_flag::IFF_NO_PI).to_raw().unwrap();
     let r = unsafe {
-        libc::ioctl(fd,TUNSETIFF, &s)
+        libc::ioctl(fd, TUNSETIFF, &s)
     };
     if r < 0 {
         println!("ioctl err");
         unsafe { libc::close(fd); }
         return;
     }
-    let mut buffer=[0u8;1024];
-    loop {
-        unsafe {
-            let nread = libc::read(fd,buffer.as_mut_ptr() as *mut c_void,1024);
-            if nread < 0 {
-                libc::close(fd);
-                break;
-            }
-            println!("read {} bytes from device",nread);
-        }
-    }
-}
-
-pub fn stream_demo() {
-    let stream = open_tuntap_device("w1".to_string()).unwrap();
-    let app = stream.for_each(|data|{
-        println!("read {} bytes from device",data.len());
-        ready(())
-    });
-    ThreadPool::new().unwrap().run(app);
+    let mut stdfile: StdFile = unsafe {
+        StdFile::from_raw_fd(fd)
+    };
+    let mut tokiofile = tokio::fs::File::from_std(stdfile);
+    let reader = tokio::codec::FramedRead::new(tokiofile, tokio::codec::BytesCodec::new());
+    //stdfile.write_all(&mut vec![1,2,3]);
+    let task = reader.for_each(|p| {
+        println!("{}", p.len());
+        Ok(())
+    }).map_err(|_| ());
+    tokio::run(task)
 }
